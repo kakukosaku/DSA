ef0a2870c133866dae3266ad296412df03351939
kaku <scugjs@gmail.com>
2018-08-18 11:50:03 +0800

    [update] bubble sort go implement.

--- go_implement/sort/bubble_sort/bubble_sort.go
+++ go_implement/sort/bubble_sort/bubble_sort.go
@@ -5,10 +5,9 @@ import (
 	"math/rand"
 )
 
-// function description you can write here.
-// func bubble(sp []int) ([]int, error) {
+// BubbleSort define: func bubble(sp []int) ([]int, error) {
 func BubbleSort(s []int) []int {
-	for i := 0; i < len(s); i += 1 {
+	for i := 0; i < len(s); i++ {
 		// 从 0 起, 从上往下冒泡...
 		// 排好序的置于最后
 		for j := 0; j < len(s)-1-i; j++ {
@@ -34,7 +33,7 @@ func main() {
 
 	// or you and do like this
 	s := make([]int, 10, 10) // len 10, cap 10 later is optional.
-	for index, _ := range s {
+	for index := range s {
 		s[index] = rand.Intn(100)
 	}
 	fmt.Println("init slice:", s)
