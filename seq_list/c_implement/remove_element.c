08ffb8d341c8469bb30d7baaa48b207bb01aa1aa
kaku <scugjs@gmail.com>
2019-04-22 08:21:06 +0800

    [add] question: remove a elem x from a sequence list

---
+++ seq_list/c_implement/remove_element.c
@@ -0,0 +1,58 @@
+//
+//  filename.c
+//  clangSkill
+//
+//  Created by kaku on 2019/04/22.
+//  Copyright © 2019 kaku. All rights reserved.
+//
+
+/*
+ * 题目描述:
+ * 对长度为n的顺序表, 编写一个时间复杂度为O(n), 空间复杂度为O(1)的算法,该算法
+ * 删除线性表中所有值为x的数据元素.
+ *
+ * Key Word:
+ * 移动删除
+ */
+
+#include "represent_array.h"
+
+#define ArraySize 10
+
+
+void del_x_1(int x, SeqList *list) {
+    int i, k = 0;
+    for (i = 0; i < list->length; i++) {
+        if (list->data[i] != x) {
+            list->data[k] = list->data[i];
+            k++;
+        } else {
+            list->length--;
+        }
+    }
+}
+
+
+void del_x_2(int x, SeqList *list) {
+    int i = 0, k = 0;
+    while (i < list->length) {
+        if (list->data[i] == x)
+            // 为了保持写书风格, 实际上我认为大括号风格更友好些 :)
+            k++, list->length--;
+        else
+            list->data[i-k] = list->data[i];
+        i++;
+    }
+}
+
+
+int main() {
+    int a[ArraySize] = {1, 2, 3, 4, 5, 4, 3, 2, 1, 0};
+    SeqList list = {a, ArraySize, ArraySize};
+    print_array(&list);
+
+    int x = 1;
+    PRINT_INT(x);
+    del_x_1(x, &list);
+    print_array(&list);
+}
---
+++ seq_list/c_implement/represent_array.h
@@ -0,0 +1,30 @@
+//
+//  filename.c
+//  clangSkill
+//
+//  Created by kaku on 2019/xx/xx.
+//  Copyright © 2019 kaku. All rights reserved.
+//
+
+#include <stdio.h>
+
+#define PRINT_INT(i) printf(#i ": %d\n", i)
+#define PRINT_CHR(c) printf(#c ": %c\n", c)
+#define PRINT_STR(s) printf(#s ": %s\n", s)
+
+
+// Sequence List Type Define
+typedef struct {
+    int *data;
+    int length, capacity;
+} SeqList;
+
+
+void print_array(SeqList *list) {
+    int i;
+    printf("Array: have \"%d\" elements.\n", list->length);
+    for (i = 0; i < list->length; i++) {
+        printf("%d ", list->data[i]);
+    }
+    printf("\n");
+}
