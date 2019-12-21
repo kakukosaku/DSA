2b03eb188857507ed091ee097a03e2720cc54a82
kaku <scugjs@gmail.com>
2019-12-21 19:33:45 +0800

    [update] Java code convention

--- leetcode/3_no_repeat_substring/no_repeat_substring.java
+++ leetcode/3_no_repeat_substring/no_repeat_substring.java
@@ -14,7 +14,7 @@ import java.util.Map;
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         Map<Character, Integer> recordMap = new HashMap<>();
-        int start_index = 0, max_no_repeat_substring_len = 0;
+        int start_index = 0, maxNoRepeatSubStringLen = 0;
         int i;
         for (i = 0; i < s.length(); i++) {
             Character checking_char = s.charAt(i);
@@ -22,14 +22,14 @@ class Solution {
             // 解决hashMap中存在早于start_index的无用值, 对判断重复子串的影响
             if (idx == null || (idx != null && idx < start_index)) {
                 recordMap.put(checking_char, i);
-                max_no_repeat_substring_len = (i - start_index + 1) > max_no_repeat_substring_len ? i - start_index + 1 : max_no_repeat_substring_len;
+                maxNoRepeatSubStringLen = (i - start_index + 1) > maxNoRepeatSubStringLen ? i - start_index + 1 : maxNoRepeatSubStringLen;
             } else {
                 start_index = recordMap.get(checking_char) + 1;
                 recordMap.put(checking_char, i);
             }
         }
 
-        return max_no_repeat_substring_len;
+        return maxNoRepeatSubStringLen;
     }
 
 
