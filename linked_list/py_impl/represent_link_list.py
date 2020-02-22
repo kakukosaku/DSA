a0fc3261b61ae2e64c35f58bcae043206e3539b6
kaku <scugjs@gmail.com>
2020-02-22 22:30:59 +0800

    [refactor] link list python impl

--- linked_list/py_impl/represent_link_list.py
+++ linked_list/py_impl/represent_link_list.py
@@ -10,74 +10,58 @@
 #
 # © 2019-2022 Kaku Kosaku All Rights Reserved
 
-
-def ensure_ele_decorator(fun):
-    """decorator for reduce redundancy code"""
-    def inner(self, data):
-        if not isinstance(data, LinkListEle):
-            data = LinkListEle(data)
-        return fun(self, data)
-    return inner
+from typing import List, Any
 
 
-class LinkListEle:
+class LinkListNode:
     """单链表 节点 元素类"""
 
     def __init__(self, data):
         self.data = data
         self.next = None
-
+    
     def __repr__(self):
-        return 'data: %s ' \
-               'next=>%s' % (self.data, self.next)
+        return "<Node: data=%s>" % self.data
 
 
 class LinkList:
 
     def __init__(self):
-        self.head = None
-        self.current_ele = None
+        self.next = None
 
-    @ensure_ele_decorator
-    def add(self, ele):
-        if self.head is None:
-            self.head = ele
-            self.current_ele = self.head
-        else:
-            self.current_ele.next = ele
-            self.current_ele = ele
+    def head_insert(self, datas: List[Any]):
+        for data in datas:
+            node = LinkListNode(data)
+            node.next = self.next
+            self.next = node
 
     def reverse(self):
-        if not self.head or not self.head.next:
-            return self.head
-        last = None
-        head = self.head
-        while head:
-            tmp = head.next
-            head.next = last
-            last = head
-            head = tmp
-        self.head = last
+        if not self.next:
+            raise ValueError("link list is null")
 
-    def get_all_ele(self):
-        cursor = self.head
-        while True:
-            print(cursor.data)
-            if not cursor.next:
-                break
-            else:
-                cursor = cursor.next
+        prev_node = None
+        curr_node = self.next
+        while curr_node:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+        
+        self.next = prev_node
 
-    def fake_present(self):
-        print(self.head)
+
+    def travel(self):
+        curr = self.next
+        while curr:
+            print(curr, end=" -> ")
+            curr = curr.next
+        print("end")
 
 
 if __name__ == '__main__':
-    ll = LinkList()
-    for i in range(0, 5):
-        ll.add(i)
-    # print(ll.head)
-    ll.get_all_ele()
-    ll.reverse()
-    print('反转链表后')
-    ll.get_all_ele()
+    datas = range(10)
+    link_list_obj = LinkList()
+    link_list_obj.head_insert(datas)
+    link_list_obj.travel()
+    link_list_obj.reverse()
+    link_list_obj.travel()
